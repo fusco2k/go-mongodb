87c1e41f260a8c785a27f3bd332982f2f1792d99
Filipe Fusco <filipe.fusco@gmail.com>
2019-04-22 18:25:35 -0300

    remove the context parameter for testing and drop the use til more profund lecture about it

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
-	"time"
+	// "time"
 )
 
+//Patient struct to receive data from DB
 type Patient struct {
-	Id        primitive.ObjectID `json:"id" bson:"_id"`
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	FName     string             `json:"fname"`
 	LName     string             `json:"lname"`
 	Email     string             `json:"email"`
@@ -24,19 +25,24 @@ type Patient struct {
 }
 
 func main() {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://192.168.122.95:27017"))
+	//get a connection creating a new client for handling it.
+	client, err := mongo.Connect(nil, options.Client().ApplyURI("mongodb://192.168.122.95:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//get a collection pointer to query
 	collection := client.Database("clinic").Collection("patient")
-	cur, err := collection.Find(ctx, bson.D{})
+
+	//get a cursor as find object
+	cur, err := collection.Find(nil, bson.D{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cur.Close(ctx)
-	
-	for cur.Next(ctx) {
+	defer cur.Close(nil)
+
+	//loop throught cursor to get the items from the collection
+	for cur.Next(nil) {
 		var result Patient
 		err := cur.Decode(&result)
 		if err != nil {
